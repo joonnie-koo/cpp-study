2ca7d27c2a8cc5cc2adc9e135a70130f664ed853
joonnie-koo <joonnie.koo@gmail.com>
2026-02-13 22:49:45 +0900

    Done

--- test-28323/src/main.cpp
+++ test-28323/src/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main (int argc, char* argv[]) {
-    cout << "Hello World" << endl;
+    int n;
+    cin >> n;
+    int answer = 0;
+    bool status;
+    vector<bool> v(n);
+    for(int i = 0; i < n; ++i){
+        int input;
+        cin >> input;
+        if(input % 2 == 0){
+            v[i] = 0;
+        }
+        else{
+            v[i] = 1;
+        }
+    }
+    status = 0;
 
-    return 0;
+    for(int i = 0; i < n; ++i){
+        if(v[i] != status){
+            ++answer;
+            status = v[i];
+        }
+    }
+    cout << answer + 1 << '\n';
 }
