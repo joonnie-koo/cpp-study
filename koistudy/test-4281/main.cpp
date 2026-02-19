83ae432e59568c873f454d53fd4f83fb726dd3c9
joonnie-koo <joonnie.koo@gmail.com>
2026-02-20 08:19:27 +0900

    Modified

--- koistudy/test-4281/main.cpp
+++ koistudy/test-4281/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main (int argc, char* argv[]) {
-    cout << "Hello World" << endl;
+vector<bool> is_prime (30000000, true);
+vector<int> prime;
 
-    return 0;
+int main (int argc, char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    if(n % 2 != 0){
+        if(n == 1){
+            cout << 2 << " " << 3 << '\n'; 
+            return 0;
+        }
+        else{
+            cout << -1 << '\n';
+        }
+    }
+    else{
+        is_prime[0] = is_prime[1] = false;
+        for(int i = 2; i * i <= 30000000; i++){
+            if(is_prime[i]){
+                for(int j = i * i; j < 30000000; j += i){
+                    is_prime[j] = false;
+                }
+            }
+        }
+        for(int i = 2; i + n <= 30000000; i++){
+            if(is_prime[i]){
+                prime.push_back(i);
+            }
+        }
+        for(int i = 0;)
+        cout << -1 << '\n';
+        return 0;
+    }
 }
