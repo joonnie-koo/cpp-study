8030929891dcf8fe3c84cf04b6bc34b7d7b8a588
joonnie-koo <joonnie.koo@gmail.com>
2026-03-24 22:54:16 +0900

    done

--- baekjoon/test-2670/src/main.cpp
+++ baekjoon/test-2670/src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main (int argc, char* argv[]) {
-    cout << "Hello World" << endl;
+    int n;
+    cin >> n;
+    double input[n];
+    double dp[n];
+    for (int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    dp[0] = input[0];
+    for (int i = 1; i < n; i++){
+        dp[i] = max(dp[i-1] * input[i], input[i]);
+    }
+    double max = 0;
+    for(int i = 0; i < n; i++){
+        if(dp[i] > max){
+            max = dp[i];
+        }
+    }
+    printf("%.3f",max);
     return 0;
 }
